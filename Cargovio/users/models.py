a9466c26151addede45a3ea5f5e7fb8cf6c49547
Karpovich <pmark2828@gmail.com>
2025-06-13 23:38:18 +0300

    feat: add base Company and Carrier models

--- Cargovio/users/models.py
+++ Cargovio/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    company_name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.company_name
+
+class Carrier(models.Model):
+    company_name = models.CharField(max_length=100)
+    vehicle_type = models.CharField(max_length=50)
+    vehicle_capacity = models.DecimalField(max_digits=10, decimal_places=2)
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.company_name} - {self.vehicle_type}"
