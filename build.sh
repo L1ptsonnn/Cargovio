8cc024d30084fcfc7c90a182d51eca0fdd3d0c21
Karpovich <pmark2828@gmail.com>
2025-06-22 16:44:12 +0300

    Fix: Set Python version via .python-version file

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Install Python 3.12.3
-pyenv install 3.12.3
-pyenv global 3.12.3
-
 pip install -r requirements.txt
 
 python Cargovio/manage.py collectstatic --no-input
