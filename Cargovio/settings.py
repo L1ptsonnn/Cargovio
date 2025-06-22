48e7fe0286114753af11df4b0df503fb113bbf63
Karpovich <pmark2828@gmail.com>
2025-06-22 16:27:40 +0300

    Fix: Add whitenoise storage and specify Python version

--- Cargovio/settings.py
+++ Cargovio/settings.py
@@ -100,6 +100,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
