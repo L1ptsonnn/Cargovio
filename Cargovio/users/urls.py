9b60c1d7c23b4f932b952ceabbcd4ec42149bc9a
Karpovich <pmark2828@gmail.com>
2025-06-22 17:45:11 +0300

    feat(ui): адаптація всіх основних сторінок для мобільних пристроїв через Bootstrap, оновлення профілю, головної, реєстрації

--- Cargovio/users/urls.py
+++ Cargovio/users/urls.py
@@ -1,18 +1,11 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='users/logout.html',
-        next_page='core:home',
-        http_method_names=['get', 'post']
-    ), name='logout'),
-
     path('register/', views.register, name='register'),
-    path('register/details/', views.register_details, name='register_details'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
 ] 
